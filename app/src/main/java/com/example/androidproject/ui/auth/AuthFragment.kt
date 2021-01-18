820f88a8bbf2607cfb33d5d10ba649c6ddf02ec2
Nugo <n_kvachadze2@cu.edu.ge>
2021-01-18 13:37:12 +0400

    Navigate to `nav_home` on Register Button click.

--- app/src/main/java/com/example/androidproject/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/androidproject/ui/auth/AuthFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.androidproject.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -25,9 +27,9 @@ class AuthFragment : Fragment() {
                 ViewModelProvider(this).get(AuthViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_auth, container, false)
 
-        val authButton: FloatingActionButton = findViewById(R.id.login)
-        // TODO NAVIGATE TO HOME PAGE ON SUCCESSFUL LOGIN
-
+        root.findViewById<Button>(R.id.login).setOnClickListener{
+            it.findNavController().navigate(R.id.action_auth_view_to_fragment_home)
+        }
 
         return root
     }
