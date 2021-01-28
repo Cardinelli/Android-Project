1dda4079a9e28526b30658c9c316e17292d55a7e
Nugo <n_kvachadze2@cu.edu.ge>
2021-01-28 23:58:38 +0400

    Implement multidex. Implement firebase registration.

--- app/src/main/java/com/example/androidproject/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/androidproject/ui/auth/AuthFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,24 +14,61 @@ import androidx.navigation.findNavController
 import com.example.androidproject.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AuthFragment : Fragment() {
-
     private lateinit var favouritesViewModel: AuthViewModel
+    private lateinit var emailInput: TextView
+    private lateinit var passwordInput: TextView
+    private lateinit var registerButton: Button
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         favouritesViewModel =
-                ViewModelProvider(this).get(AuthViewModel::class.java)
+            ViewModelProvider(this).get(AuthViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_auth, container, false)
 
-        root.findViewById<Button>(R.id.login).setOnClickListener{
-            it.findNavController().navigate(R.id.action_auth_view_to_fragment_home)
+        registerButton = root.findViewById(R.id.register_btn)
+        emailInput = root.findViewById(R.id.username)
+        passwordInput = root.findViewById(R.id.password)
+
+        auth = Firebase.auth
+        auth.currentUser?.let {
+            Toast.makeText(context, "Hello ${it.uid}", Toast.LENGTH_SHORT).show()
         }
 
+        registerButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            register(email, password)
+
+            it.findNavController().navigate(R.id.action_auth_view_to_fragment_home)
+        }
         return root
+    }
+
+    private fun register(email: String, password: String) {
+        activity?.let {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(it) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(context, "Hello ${user?.uid}", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            context, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
